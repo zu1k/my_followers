625b894540e542c4f8eb23ab2692cc02e0365075
zu1k <i@zu1k.com>
2023-03-27 22:19:38 +0800

    Update imgs.py

--- imgs.py
+++ imgs.py
@@ -153,6 +153,7 @@ if __name__ == '__main__':
     user = os.getenv("USER")
     get_followers(token, user)
     download_imgs()
+    
     # svg maybe greater than 100MB
     # gen_svg(128, 0, 34)
     composite_image()
